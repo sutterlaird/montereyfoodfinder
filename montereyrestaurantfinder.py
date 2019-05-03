e6d2e35f7a7ed08db2eaddca300869f5b6f0c997
Hector Lopez <element351@gmail.com>
2019-05-02 23:43:34 -0700

    updated

--- montereyrestaurantfinder.py
+++ montereyrestaurantfinder.py
@@ -3,6 +3,11 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
+from PyQt5.QtGui import QColor
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QGridLayout, QGroupBox,
+                             QMenu, QPushButton, QRadioButton, QVBoxLayout, QWidget, QSlider)
+
 
 
 # ChooseGenreForm is the class for the form that the user uses to select the genre for a random restaurant
@@ -15,14 +20,11 @@ class ChooseGenreForm(FlaskForm):
         'Select Genre',
         choices=[('american', 'American'), ('mexican', 'Mexican'), ('italian', 'Italian'), ('mediterranean', 'Mediterranean')]
     )
-    submit=SubmitField("GO!")
+    submit=SubmitField('Find me food!')
     
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-
-
-
 
 
 # Render the homepage with the form for the genre selection
@@ -32,17 +34,11 @@ def home():
     return render_template('home.html', form=form)
 
 
-
-
-
 # Display the results of the genre form. So far, just shows the genre very large
 @app.route('/submit', methods=('GET', 'POST'))
 def submit():
     form = ChooseGenreForm()
     return render_template('randomSelection.html', genre=dict(form.genres.choices).get(form.genres.data))
-
-
-
 
 
 # This is necessary according to StackOverflow. Not sure why or what it does
