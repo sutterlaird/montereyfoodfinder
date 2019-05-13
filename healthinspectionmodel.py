0b0eefbbbf0b772dc25c1e0623d4087669832077
Sutter <sutter@Sutters-iMac.local>
2019-05-13 11:02:24 -0700

    Changes to health inspection information

--- healthinspectionmodel.py
+++ healthinspectionmodel.py
@@ -18,16 +18,37 @@ class healthinspectionmodel():
             result = cursor.fetchone()
             if result:
                 return str(result[0])
-            else:
-                return "not found"
+            # If no success, adjust the query and try again
+
+            # Remove apostrophes
+            noApostrophes = restaurantName.replace("'", "")
+            sql = "SELECT `permit_no` FROM `restaurants` WHERE `name`LIKE %s"
+            cursor.execute(sql, ("%" + noApostrophes + "%"))
+            # Get the first result
+            result = cursor.fetchone()
+            if result:
+                return str(result[0])
+
+            # Try just first two words
+            nameList = restaurantName.split()
+            if len(nameList) > 1:
+                sql = "SELECT `permit_no` FROM `restaurants` WHERE `name`LIKE %s"
+                cursor.execute(sql, ("%" + nameList[0] + " " + nameList[1] + "%"))
+                result = cursor.fetchone()
+                if result:
+                    return str(result[0])
+            return "not found"
 
     def getRestaurantInformation(self, restaurantName):
+        inspectionData = list()
         # Get permit number for the restaurant
         permit_num = self.getRestaurantPermitNum(restaurantName)
         # If not in DB, abort immediately
         if permit_num == "not found":
-            return "Health Inspection Data Not Available"
+            inspectionData.append(False)
+            return inspectionData
         # Otherwise, proceed with extracting data
+        inspectionData.append(True)
         inspection_site = "http://www.decadeonline.com/insp.phtml?agency=mon&violsortfield=TB_CORE_INSPECTION_VIOL.VIOLATION_CODE&record_id=PR0"
         # Request headers
         req = Request(
@@ -40,12 +61,18 @@ class healthinspectionmodel():
         soup = BeautifulSoup(resp.read(), 'lxml')
         # Find all <td> elements with class="inspectionTableHeading"
         inspections = soup.findAll("td", {"class": "inspectionTableHeading"})
-        outputString = "Most Recent Inspection"
+        # outputString = "Most Recent Inspection"
         # The code for scraping the inspection page is ugly and hacky but it works
         # First process the column that shows the date and type of inspection
         typeList = str(inspections[0]).split("<")
-        outputString += "\n" + typeList[2][28:]
+        inspectionData.append(typeList[2][28:])
         # Then process the column that shows the inspection results
         inspectList = str(inspections[1]).split(">")
-        outputString += "\n" + inspectList[1][:-5]
-        return outputString
+        inspectionData.append(inspectList[1][:-5])
+        inspectionData.append(inspection_site + str(permit_num))
+        return inspectionData
+
+
+# TEST CODE PLEASE REMOVE
+# healthmodel = healthinspectionmodel()
+# print(healthmodel.getRestaurantInformation("Tacos California Grill Mexican Restaurant"))
--- permit_number_scraper.py
+++ permit_number_scraper.py
@@ -7,10 +7,10 @@ cursor = db.cursor()
 
 root_url = "http://www.decadeonline.com/insp.phtml?agency=mon&violsortfield=TB_CORE_INSPECTION_VIOL.VIOLATION_CODE&record_id=PR0"
 
-id = 609999
+id = 600000
 # id = 612513
 
-while id < 700000:
+while id < 640000:
     # Request headers
     req = Request(
         root_url + str(id),
@@ -27,21 +27,30 @@ while id < 700000:
         if "939" in str(boldText):
             # Remove the tag brackets
             nameList = str(boldText).split("<")
+            nameList[1] = nameList[1].replace("'", "''")
+            restaurantName = nameList[1][4:]
             # Print the permit number and the name with the <b> removed
-            print(str(id) + ": " + nameList[1][4:])
-            # Insert the permit number and the restaurant name into the database
-            sql = "INSERT INTO restaurants(permit_no, \
-            name) \
-            VALUES ('%d', '%s')" % \
-            (id, nameList[1][4:])
-            try:
-                # Execute the SQL command
-                cursor.execute(sql)
-                # Commit your changes in the database
-                db.commit()
-            except:
-                # Rollback in case there is any error
-                db.rollback()
+            print(str(id) + ": " + restaurantName)
+            sql = "SELECT * FROM `restaurants` WHERE `permit_no`=%d"
+            cursor.execute(sql % id)
+            result = cursor.fetchone()
+            if result:
+                print("Duplicate")
+            else:
+                # Insert the permit number and the restaurant name into the database
+                sql = "INSERT INTO restaurants(permit_no, \
+                name) \
+                VALUES ('%d', '%s')" % \
+                (id, restaurantName)
+                try:
+                    # Execute the SQL command
+                    cursor.execute(sql)
+                    # Commit your changes in the database
+                    db.commit()
+                except pymysql.InternalError as error:
+                    print("Failed to add to DB" + error)
+                    # Rollback in case there is any error
+                    db.rollback()
     id += 1
 
 
